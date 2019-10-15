65f9c06f93242b051d49432113ac04d811ea3380
Jasmine <jasminecharles@hotmail.com>
2019-10-15 15:25:27 -0700

    login form, auth

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
 import './App.css';
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h1>Auth-Friends</h1>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/private">Private Page</Link>
+        </li>
+        
+      </ul>
+      <Switch>
+      
+      <PrivateRoute path="/private" component={FriendsList}/>
+      <Route path="login" component={LoginForm}/>
+      <Route component={LoginForm}/>
+      
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = props => {
+
+    return (
+    <Route />
+    )
+
+}
+export default PrivateRoute;
